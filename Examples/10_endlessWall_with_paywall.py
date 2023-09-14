6ada40097d188b8ef2ce061c8701c088af1d3de7
maximezouzou <104771834+maximezouzou@users.noreply.github.com>
2023-09-14 12:21:38 +0200

    Guideline update

--- Examples/10_endlessWall_with_paywall.py
+++ Examples/10_endlessWall_with_paywall.py
@@ -4,22 +4,22 @@ import smartpy as sp
 def main():
 
     class EndlessWall(sp.Contract):
-       def __init__(self, initialText, owner):
-           self.data.wallText = initialText
-           self.data.nbCalls = 0
+       def __init__(self, initial_text, owner):
+           self.data.wall_text = initial_text
+           self.data.nb_calls = 0
            self.data.owner = owner
     
        @sp.entrypoint
        def write_message(self, message):
            assert (sp.len(message) <= 30) and (sp.len(message) >= 3), "invalid size message"
            assert sp.amount == sp.tez(1), "incorrect amount"
-           self.data.wallText += ", " + message + " forever"
-           self.data.nbCalls += 1
+           self.data.wall_text += ", " + message + " forever"
+           self.data.nb_calls += 1
     
        @sp.entrypoint
-       def claim(self, requestedAmount):
+       def claim(self, requested_amount):
             assert sp.sender == self.data.owner, "not your money"
-            sp.send(self.data.owner, requestedAmount)
+            sp.send(self.data.owner, requested_amount)
     
        @sp.entrypoint
        def claim_all(self):
@@ -32,20 +32,20 @@ def test():
    alice=sp.test_account("Alice").address
    bob=sp.test_account("Bob").address
    eve=sp.test_account("Eve").address
-   c1 = main.EndlessWall(initialText = "Axel on Tezos forever", owner=alice)
+   c1 = main.EndlessWall(initial_text = "Axel on Tezos forever", owner=alice)
    scenario = sp.test_scenario(main)
    scenario += c1
    scenario.h3(" Testing write_message is ok ")
    c1.write_message("Ana & Jack").run(sender = eve, amount = sp.tez(1))
    c1.write_message("freeCodeCamp").run(sender = bob, amount = sp.tez(1))
-   scenario.verify(c1.data.wallText == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
+   scenario.verify(c1.data.wall_text == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
    scenario.h3(" Checking calls fail due to invalid size message ")
    c1.write_message("this message is 31 letters long").run(sender = alice, valid = False, amount = sp.tez(1))
    c1.write_message("AB").run(sender = alice, valid = False, amount = sp.tez(1))
    scenario.h3(" Checking calls pass for limit size messages ")
    c1.write_message("LLL").run(sender = alice, amount = sp.tez(1))
    c1.write_message("this message has 30 characters").run(sender = eve, amount = sp.tez(1) )
-   scenario.verify(c1.data.nbCalls == 4)
+   scenario.verify(c1.data.nb_calls == 4)
    scenario.h3(" Checking calls pass or fail according to the amounts")
    c1.write_message("testing right amount").run(sender = eve,amount = sp.tez(1))
    c1.write_message("testing lesser amount").run(sender = eve,amount = sp.mutez(999999), valid = False)
