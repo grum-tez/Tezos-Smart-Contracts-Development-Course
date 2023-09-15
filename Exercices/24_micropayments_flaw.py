acbe5afefaaf8c50ab13cb1f894f178b183232ba
maximezouzou <104771834+maximezouzou@users.noreply.github.com>
2023-09-15 10:55:21 +0200

    Guideline updates

--- Exercices/24_micropayments_flaw.py
+++ Exercices/24_micropayments_flaw.py
@@ -11,14 +11,14 @@ def main():
         def deposit(self, destination, deadline, public_key):
             key = sp.record(source = sp.sender, destination = destination)
             if not self.data.deposits.contains(key):
-                self.data.deposits[key] = sp.record(deadline=deadline, amount=sp.amount, public_key = public_key)
+                self.data.deposits[key] = sp.record(deadline = deadline, amount = sp.amount, public_key = public_key)
             else:
                 assert self.data.deposits[key].deadline < deadline
                 self.data.deposits[key].deadline = deadline
                 self.data.deposits[key].amount += sp.amount
 
         @sp.entrypoint
-        def claimPayments(self, source, packed_message, signature):
+        def claim_payments(self, source, packed_message, signature):
             key = sp.record(source = source, destination = sp.sender)
             deposit = self.data.deposits[key]
             assert sp.check_signature(deposit.public_key, signature, packed_message)
@@ -29,14 +29,14 @@ def main():
             sp.send(sp.sender, message.amount)
 
         @sp.entrypoint
-        def closeAccount(self, destination):
+        def close_account(self, destination):
             key = sp.record(source = sp.sender, destination = destination)
             assert self.data.deposits[key].deadline <= sp.now
             sp.send(sp.sender, self.data.deposits[key].amount)
             del self.data.deposits[key]
                    
 
-@sp.add_test(name='Testing extortion attack')
+@sp.add_test(name = "Testing extortion attack")
 def test():
     alice = sp.test_account("alice")
     bob = sp.test_account("bob").address
@@ -48,7 +48,7 @@ def test():
     message = sp.record(destination = bob, amount = sp.tez(5))
     packed_message = sp.pack(message)
     signature = sp.make_signature(alice.secret_key, packed_message)
-    micropayments.claimPayments(source = alice.address, packed_message = packed_message, signature = signature).run(sender = bob)
+    micropayments.claim_payments(source = alice.address, packed_message = packed_message, signature = signature).run(sender = bob)
 
             
     
