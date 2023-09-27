82e03a2a553517deb80353a83db7d0d6af710db0
mathias-hiron <mathias.hiron@gmail.com>
2023-09-27 09:34:42 +0200

    Update to new syntax

--- Examples/04_flip_value_entrypoint_call_in_test_scenario.py
+++ Examples/04_flip_value_entrypoint_call_in_test_scenario.py
@@ -11,9 +11,9 @@ def main():
         def flip(self):
             self.data.best_digit = 1 - self.data.best_digit
 
-@sp.add_test(name = "Testing")
+@sp.add_test()
 def test():
-    scenario = sp.test_scenario(main)
+    scenario = sp.test_scenario("Test", main)
     contract = main.FlipValue()
     scenario += contract
     scenario.h2("Testing flip entrypoint")
