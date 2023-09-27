48806e8d10f9bc6fe8a77028f86f07d4a3a8be8c
mathias-hiron <mathias.hiron@gmail.com>
2023-09-27 10:03:43 +0200

    Update to new syntax

--- Examples/13_endlessWall_with_deadline.py
+++ Examples/13_endlessWall_with_deadline.py
@@ -23,48 +23,48 @@ def main():
             assert sp.sender == self.data.owner, "not your money"
             sp.send(self.data.owner, requested_amount)     
   
-@sp.add_test(name = "Add my name")
+@sp.add_test()
 def test():
-    alice=sp.test_account("Alice").address
-    bob=sp.test_account("Bob").address
-    eve=sp.test_account("Eve").address
+    alice = sp.test_account("Alice").address
+    bob = sp.test_account("Bob").address
+    eve = sp.test_account("Eve").address
     c1 = main.EndlessWall(initial_text = "Axel on Tezos forever", owner=alice, deadline = sp.timestamp_from_utc(2025, 12, 31, 23, 59, 59))
-    sc = sp.test_scenario(main)
+    sc = sp.test_scenario("Test", main)
     sc += c1
     sc.h3(" Testing write_message is ok ")
     #sc write_message ok
-    c1.write_message("Ana & Jack").run(sender = eve, amount = sp.tez(1), now = sp.timestamp(0) )
-    c1.write_message("freeCodeCamp").run(sender = bob, amount = sp.tez(1), now = sp.timestamp(0) )
+    c1.write_message("Ana & Jack", _sender = eve, _amount = sp.tez(1), _now = sp.timestamp(0) )
+    c1.write_message("freeCodeCamp", _sender = bob, _amount = sp.tez(1), _now = sp.timestamp(0) )
     sc.verify(c1.data.wall_text == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
     sc.h3(" Checking calls fail due to invalid size message ")
     #checking write_message fails for size message
-    c1.write_message("this message is 31 letters long").run(sender = alice, valid = False, amount = sp.tez(1))
-    c1.write_message("AB").run(sender = alice, valid = False, amount = sp.tez(1))
+    c1.write_message("this message is 31 letters long", _sender = alice, _valid = False, _amount = sp.tez(1))
+    c1.write_message("AB", _sender = alice, _valid = False, _amount = sp.tez(1))
     sc.h3(" Checking calls pass for limit size messages ")
     #checking write_message passes for size message
-    c1.write_message("LLL").run(sender = alice, amount = sp.tez(1))
-    c1.write_message("this message has 30 characters").run(sender = eve, amount = sp.tez(1) )
+    c1.write_message("LLL", _sender = alice, _amount = sp.tez(1))
+    c1.write_message("this message has 30 characters", _sender = eve, _amount = sp.tez(1) )
     sc.verify(c1.data.nb_calls == 4)
     sc.h3(" Checking calls pass or fail for right amount")
     #checking testing amounts
-    c1.write_message("testing right amount").run(sender = eve,amount = sp.tez(1))
-    c1.write_message("testing lesser amount").run(sender = eve,amount = sp.mutez(999999), valid = False)
-    c1.write_message("testing bigger amount").run(sender = bob, amount = sp.mutez(1000001), valid = False)
-    c1.write_message("testing correct amount").run(sender = bob, amount = sp.tez(1))
+    c1.write_message("testing right amount", _sender = eve, _amount = sp.tez(1))
+    c1.write_message("testing lesser amount", _sender = eve, _amount = sp.mutez(999999), _valid = False)
+    c1.write_message("testing bigger amount", _sender = bob, _amount = sp.mutez(1000001), _valid = False)
+    c1.write_message("testing correct amount", _sender = bob, _amount = sp.tez(1))
     sc.h3(" Checking only owner can claim balance in the contract")
     #checking claim entrypoint
-    c1.claim(sp.tez(3)).run(sender = bob, valid = False)
-    c1.claim(sp.tez(4)).run(sender = alice)
+    c1.claim(sp.tez(3), _sender = bob, _valid = False)
+    c1.claim(sp.tez(4), _sender = alice)
     sc.h3(" Checking deadline passes or fails")
     #checking deadline
-    c1.write_message("Bob wrote this").run(
-        sender=bob,
-        amount=sp.tez(1),
-        now=sp.timestamp_from_utc(2025, 12, 31, 23, 59, 58),
+    c1.write_message("Bob wrote this",
+        _sender = bob,
+        _amount = sp.tez(1),
+        _now = sp.timestamp_from_utc(2025, 12, 31, 23, 59, 58),
     )
-    c1.write_message("Eve wrote this").run(
-        sender=eve,
-        amount=sp.tez(1),
-        now=sp.timestamp_from_utc(2026, 1, 1, 0, 0, 1),
-        valid=False,
+    c1.write_message("Eve wrote this",
+        _sender = eve,
+        _amount = sp.tez(1),
+        _now = sp.timestamp_from_utc(2026, 1, 1, 0, 0, 1),
+        _valid = False,
     )
