f5ea692e581fe051c0db824e9df4cc2e315e340b
mathias-hiron <mathias.hiron@gmail.com>
2023-09-28 09:36:27 +0200

    Update to new syntax

--- Exercices/10_single_basic_nft.py
+++ Exercices/10_single_basic_nft.py
@@ -6,23 +6,23 @@ def main():
     class SingleBasicNFT(sp.Contract):
       def __init__(self, first_owner):
           self.data.owner = first_owner
-          self.data.metadata = "Gwen's first NFT"
+          self.data.metadata = "My first NFT"
     
       @sp.entry_point
       def transfer(self, new_owner):
           assert sp.sender == self.data.owner, "not your property"
           self.data.owner = new_owner
 
-@sp.add_test(name = "update owner")
+@sp.add_test()
 def test():
     alice = sp.test_account("alice").address
     bob = sp.test_account("bob").address
     eve = sp.test_account("eve").address
     c1 = main.SingleBasicNFT(alice)
-    scenario = sp.test_scenario(main)
+    scenario = sp.test_scenario("Test", main)
     scenario += c1
-    scenario.h3(" Testing transfer entrypoint")
-    c1.transfer(bob).run(sender = alice)
-    c1.transfer(eve).run(sender = bob)
-    c1.transfer(alice).run(sender = eve)
+    scenario.h3("Testing transfer entrypoint")
+    c1.transfer(bob, _sender = alice)
+    c1.transfer(eve, _sender = bob)
+    c1.transfer(alice, _sender = eve)
     scenario.verify(c1.data.owner == alice)
