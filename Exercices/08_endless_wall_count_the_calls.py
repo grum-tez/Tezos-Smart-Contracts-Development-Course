a2c51dc5e6972e107f939779581dd0ed946cece2
mathias-hiron <mathias.hiron@gmail.com>
2023-09-28 09:27:22 +0200

    Update to new syntax

--- Exercices/08_endless_wall_count_the_calls.py
+++ Exercices/08_endless_wall_count_the_calls.py
@@ -10,24 +10,24 @@ def main():
         
         @sp.entry_point
         def write_message(self, message):
-            assert (sp.len(message) <= 30) and (sp.len(message) >= 3), "message size invalid"
+            assert (sp.len(message) <= 30) and (sp.len(message) >= 3), "invalid length"
             self.data.wall_text += ", " + message + " forever"
             self.data.nb_calls += 1
   
-@sp.add_test(name = "Add my name")
+@sp.add_test()
 def test():
-   c1 = main.EndlessWall(initial_text = "Axel on Tezos forever")
-   scenario = sp.test_scenario(main)
+   c1 = main.EndlessWall(initial_text = "Hello")
+   scenario = sp.test_scenario("Test", main)
    scenario += c1
    scenario.h3(" Testing write_message")
    c1.write_message("Ana & Jack")
-   c1.write_message("freeCodeCamp")
-   scenario.verify(c1.data.wall_text == "Axel on Tezos forever, Ana & Jack forever, freeCodeCamp forever")
+   c1.write_message("Tezos")
+   scenario.verify(c1.data.wall_text == "Hello, Ana & Jack forever, Tezos forever")
    scenario.verify(c1.data.nb_calls == 2)
    scenario.h3(" Testing write_message size message on limits")
-   c1.write_message("this message is 31 letters long").run(valid = False)
-   c1.write_message("AB").run(valid = False)
-   #by default a transaction is valid, no need to add .run(valid = True) after testing a call that should be a valid transaction
+   c1.write_message("this message is 31 letters long", _valid = False)
+   c1.write_message("AB", _valid = False)
+   #by default a transaction is valid, no need to add _valid = True after testing a call that should be a valid transaction
    c1.write_message("LLL")
    c1.write_message("this message is 30 characters ")
    scenario.verify(c1.data.nb_calls == 4)
