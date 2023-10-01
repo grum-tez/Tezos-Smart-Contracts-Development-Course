2503ec42bf0aad42040f9447458f4f88030edf6e
mathias-hiron <mathias.hiron@gmail.com>
2023-10-01 22:33:20 +0200

    Make identical to the slide

--- Examples/02_store_value_with_test_scenario.py
+++ Examples/02_store_value_with_test_scenario.py
@@ -1,13 +1,13 @@
 import smartpy as sp
+
 @sp.module
 def main():
-
     class StoreValue(sp.Contract):
         def __init__(self):
-            self.data.stored_value = 42
+            self.data.value = 342185
 
 @sp.add_test()
 def test():
-    scenario = sp.test_scenario("Test", main)
+    scenario = sp.test_scenario("test", main)
     contract = main.StoreValue()
     scenario += contract
