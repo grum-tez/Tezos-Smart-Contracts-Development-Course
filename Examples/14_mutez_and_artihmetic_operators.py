41100639fbfb80128882cb1de2296ad74bfd2514
mathias-hiron <mathias.hiron@gmail.com>
2023-10-04 10:08:43 +0200

    Update to new syntax

--- Examples/14_mutez_and_artihmetic_operators.py
+++ Examples/14_mutez_and_artihmetic_operators.py
@@ -1,5 +1,4 @@
 import smartpy as sp
-from utils import utils
 
 @sp.module
 def main():
@@ -16,25 +15,24 @@ def main():
         @sp.entrypoint
         def sub(self, x, y):
             self.data.financial_value = utils.nat_to_mutez(x) - utils.nat_to_mutez(y)
-            #WARNING sp.tez(<natural number>), sp.mutez(<natural number>): No negative value
        
         @sp.entrypoint
         def multiply(self, x, y):
+            sp.cast(x, sp.mutez)
+            sp.cast(y, sp.nat)
             self.data.financial_value = sp.mul(x, y)
-            #x being of type mutez and y of type nat
             
         @sp.entrypoint
         def divide(self, x, y):
+            sp.cast(x, sp.mutez)
+            sp.cast(y, sp.nat)
             self.data.financial_value = sp.fst(sp.ediv(x, y).unwrap_some())
-            #x being of type mutez and y of type nat
-            #it returns and option of a pair and you can use open_some() to open the option
-            # and then sp.fst to get the first element of the pair and sp.snd to get the rest
 
 
-@sp.add_test(name = "Testing operations")
+@sp.add_test()
 def test():
     c1 = main.ComputingTez()
-    sc = sp.test_scenario([utils,main])
+    sc = sp.test_scenario("Test", [sp.utils,main])
     sc += c1
     c1.add(x = 5, y = 3)
     c1.sub(x = 5, y = 3)
