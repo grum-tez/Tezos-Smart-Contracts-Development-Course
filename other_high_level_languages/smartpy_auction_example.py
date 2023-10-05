e86453d1afacb8f57c2b78efd5e8a99d1f6e8e69
Axel Roffi <axel.roffi@gmail.com>
2023-10-05 17:21:06 +0200

    Update smartpy_auction_example.py

--- other_high_level_languages/smartpy_auction_example.py
+++ other_high_level_languages/smartpy_auction_example.py
@@ -1,30 +1,30 @@
 import smartpy as sp
 
+@sp.module
+def main():
     class Auction(sp.Contract):
         def __init__(self, owner, deadline):
-            self.init(owner = owner, deadline = deadline, topBidder = owner,
+            self.data = sp.record(owner = owner, deadline = deadline, top_bidder = owner,
                 bids = { owner: sp.tez(0) })
     
         @sp.entry_point
         def bid(self):
-            sp.verify(sp.now < self.data.deadline, "Too late!")
-            bids = self.data.bids
-            sp.verify(~bids.contains(sp.sender), "You already bid")
-            bids[sp.sender] = sp.amount
-            sp.if sp.amount > bids[self.data.topBidder]:
-                self.data.topBidder = sp.sender
+            assert sp.now < self.data.deadline, "Too late!"
+            assert not self.data.bids.contains(sp.sender), "You already bid"
+            self.data.bids[sp.sender] = sp.amount
+            if sp.amount > self.data.bids[self.data.top_bidder]:
+                self.data.top_bidder = sp.sender
     
         @sp.entry_point
-        def collectTopBid(self):
-            sp.verify(sp.now >= self.data.deadline, "Too early!")
+        def Collect_top_bid(self):
+            assert sp.now >= self.data.deadline, "Too early!"
             sp.verify(sp.sender == self.data.owner)
-            sp.send(sp.sender, self.data.bids[self.data.topBidder])
-            del self.data.bids[self.data.topBidder]
+            assert sp.sender = self.data.owner, "Not the owner"
+           sp.send(sp.sender, self.data.bids[self.data.top_bidder]
         
         @sp.entry_point
         def claim(self):
-            sp.verify(self.data.bids.contains(sp.sender))
-            sp.verify(sp.now >= self.data.deadline, "To early!")
-            sp.verify(sp.sender != self.data.topBidder, "You won the auction")
-            sp.send(sp.sender, self.data.bids[sp.sender])
+            assert sp.now >= self.data.deadline, "Too early!"
+            assert sp.sender != self.data.top_bidder, "You won the auction"
+            sp.send(sp.sender, self.data.bids.get(sp.sender, error = "Not a bidder")
             del self.data.bids[sp.sender]
