55fa0737dfde6fbdf2124b38faf47e577fa45008
mathias-hiron <mathias.hiron@gmail.com>
2023-10-05 15:53:37 +0200

    Update type inference exercise

--- Exercices/03_store_value_find_bug_and_fix_it.py
+++ Exercices/03_store_value_find_bug_and_fix_it.py
@@ -1,28 +1,30 @@
-#Exercise: find out why we have an error here, and how to fix it. 
-#Note that we don’t want to be able to use add to decrease the value, 
-#or to use sub to increase it.
-
 import smartpy as sp
 
 @sp.module
 def main():
 
     class StoreValue(sp.Contract):
-       def __init__(self):
-           self.data.store_value = sp.int(24)
+       def __init__(self, starting_value):
+           self.data.stored_value = starting_value
     
        @sp.entrypoint
-       def add(self, a):
-          self.data.store_value += a
+       def add(self, added_value):
+          sp.cast(added_value, sp.nat) # We don't want to allow negative numbers
+          self.data.stored_value += added_value
+
+       @sp.entrypoint
+       def sub(self, subtracted_value):
+          self.data.stored_value -= subtracted_value
     
        @sp.entrypoint
-       def sub(self, b):
-           self.data.store_value -= b
+       def reset(self):
+           self.data.stored_value = 0
 
 @sp.add_test()
 def test():
    scenario = sp.test_scenario("Test", main)
-   contract = main.StoreValue()
+   contract = main.StoreValue(42)
    scenario += contract
-   contract.add(sp.nat(5))
+   scenario.h3("Type inference")
+   contract.add(sp.int(5))
    contract.sub(5)
