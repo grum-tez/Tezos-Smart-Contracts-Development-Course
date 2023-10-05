4aa497e912fe62ef86a89ef3e62d5fdfcb3827ba
mathias-hiron <mathias.hiron@gmail.com>
2023-10-05 15:50:58 +0200

    Update exercise on type inference

--- Examples/06_store_value_help_type_inference.py
+++ Examples/06_store_value_help_type_inference.py
@@ -4,13 +4,17 @@ import smartpy as sp
 def main():
 
     class StoreValue(sp.Contract):
-       def __init__(self):
-           self.data.stored_value = sp.nat(42)
+       def __init__(self, starting_value):
+           self.data.stored_value = starting_value
     
        @sp.entrypoint
        def add(self, added_value):
-          sp.cast(added_value, sp.nat)
+          sp.cast(added_value, sp.nat) # We don't want to allow negative numbers
           self.data.stored_value += added_value
+
+       @sp.entrypoint
+       def sub(self, subtracted_value):
+          self.data.stored_value -= subtracted_value
     
        @sp.entrypoint
        def reset(self):
@@ -19,7 +23,8 @@ def main():
 @sp.add_test()
 def test():
    scenario = sp.test_scenario("Test", main)
-   contract = main.StoreValue()
+   contract = main.StoreValue(42)
    scenario += contract
-   scenario.h3("Helping with type inference")
-   contract.add(sp.nat(5))
+   scenario.h3("Type inference")
+   contract.add(sp.int(5))
+   contract.sub(5)
