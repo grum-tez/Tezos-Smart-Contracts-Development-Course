71445a462b81a89a5e95a0eaf2ace0de8801996f
mathias-hiron <mathias.hiron@gmail.com>
2023-10-21 15:19:24 +0200

    Update to new syntax + fix

--- Exercices/17_true_endless_wall.py
+++ Exercices/17_true_endless_wall.py
@@ -4,35 +4,59 @@ import smartpy as sp
 def main():
     class TrulyEndlessWall(sp.Contract):
         def __init__(self, owner):
-            self.data.messages = sp.cast(sp.big_map({}),
-                                         sp.big_map[sp.address,
-                                                    sp.record(text = sp.string,
-                                                              timestamp = sp.timestamp)
-                                         ]
-                                        )
-            self.data.owner = owner
-           
+            self.data.messages = sp.big_map({})
+            sp.cast(self.data.messages, sp.big_map[sp.address, sp.record(text = sp.string,
+                                                                         timestamp = sp.timestamp)])
      
         @sp.entrypoint
-        def write_message(self, text, timestamp):
-           data = sp.record(text = "", timestamp = sp.timestamp(0))
+        def write_message(self, text):
+           data = sp.record(text = "", timestamp = sp.now)
            if self.data.messages.contains(sp.sender):
                data = self.data.messages[sp.sender]
                data.text += "," + text
            else:
                data.text = text
-           data.timestamp = timestamp
+           data.timestamp = sp.now
            self.data.messages[sp.sender] = data
     
        
-@sp.add_test(name = "testing truly endless wall")
+@sp.add_test()
 def test():
     alice = sp.test_account("Alice").address
     bob = sp.test_account("Bob").address
     eve = sp.test_account("Eve").address
-    axel = sp.test_account("Axel").address
-    scenario = sp.test_scenario(main)
-    c1 = main.TrulyEndlessWall(axel)
+    scenario = sp.test_scenario("Test", main)
+    c1 = main.TrulyEndlessWall(alice)
     scenario += c1
-    c1.write_message(text="bob's message 1", timestamp=sp.timestamp(20)).run(sender= bob)
-    c1.write_message(text="bob's message 2", timestamp=sp.timestamp(30)).run(sender= bob)
+    c1.write_message(text="bob's message 1", _sender= bob)
+    c1.write_message(text="bob's message 2"import smartpy as sp
+
+@sp.module
+def main():
+    class TrulyEndlessWall(sp.Contract):
+        def __init__(self, owner):
+            self.data.messages = sp.big_map({})
+            sp.cast(self.data.messages, sp.big_map[sp.address, sp.record(text = sp.string, timestamp = sp.timestamp)])
+     
+        @sp.entrypoint
+        def write_message(self, text):
+           data = sp.record(text = "", timestamp = sp.now)
+           if self.data.messages.contains(sp.sender):
+               data = self.data.messages[sp.sender]
+               data.text += "," + text
+           else:
+               data.text = text
+           data.timestamp = sp.now
+           self.data.messages[sp.sender] = data
+    
+       
+@sp.add_test()
+def test():
+    alice = sp.test_account("Alice").address
+    bob = sp.test_account("Bob").address
+    eve = sp.test_account("Eve").address
+    scenario = sp.test_scenario("Test", main)
+    c1 = main.TrulyEndlessWall(alice)
+    scenario += c1
+    c1.write_message(text="bob's message 1", timestamp=sp.timestamp(20), _sender= bob)
+    c1.write_message(text="bob's message 2", timestamp=sp.timestamp(30), _sender= bob)
