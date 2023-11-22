11f77a49f03a73122c32ec656741b1a46db2bd66
Axel Roffi <axel.roffi@gmail.com>
2023-11-22 09:59:03 +0100

    Update membership.py

--- Big_exercise/membership.py
+++ Big_exercise/membership.py
@@ -2,27 +2,46 @@ import smartpy as sp
 
 @sp.module
 def main():
-  
     class Membership(sp.Contract):
-        def __init__(self, membership_price, owner, ledger, liquidity_pool):
-            self.data.owner = owner
-            self.data.membership_price = membership_price
+        def __init__(self, membership_threshold):
+            self.data.membership_threshold = membership_threshold
             self.data.members = sp.set()
-            self.data.ledger = ledger
-            self.data.liquidity_pool = liquidity_pool
 
         @sp.entrypoint
         def join(self):
-            assert sp.amount == self.data.membership_price
+            assert sp.amount == self.data.membership_threshold
+            assert len(self.data.members) < 100
             self.data.members.add(sp.sender)
-            sp.send(self.data.owner, sp.amount)
-            self.data.members.add(sp.sender)
+            sp.send(sp.sender, sp.amount)
+
+        @sp.onchain_view
+        def is_member(self, user):
+            sp.cast(user, sp.address)
+            return self.data.members.contains(user)
 
         @sp.entrypoint
-        def join_with_tokens(self, nb_tokens):
-            sp.cast(nb_tokens, sp.nat)
-            token_price =  sp.view("get_token_price", self.data.liquidity_pool, (), sp.mutez).unwrap_some();
-            assert sp.mul(token_price, nb_tokens) > self.data.membership_price
-            ledger_contract = sp.contract(param_type, self.data.ledger, entrypoint="transfer").unwrap_some()
-            sp.transfer(sp.record(source = sp.sender, destination = self.data.owner, amount = nb_tokens), sp.tez(0), ledger_contract)
-            self.data.members.add(sp.sender)
+        def leave(self):
+            self.data.members.remove(sp.sender)
+        
+
+@sp.add_test()
+def test():
+    alice = sp.test_account("Alice")
+    bob = sp.test_account("Bob")
+    carl = sp.test_account("Carl")
+    
+    scenario = sp.test_scenario("Test", main)
+
+    # Init NftMarketplace
+    rich_community = main.Membership(sp.tez(1000))
+
+    scenario += rich_community
+
+    rich_community.join(_sender = alice, _amount = sp.tez(1000))
+    rich_community.join(_sender = bob, _amount = sp.tez(9999), _valid = False)
+    rich_community.join(_sender = bob, _amount = sp.tez(1000), _valid = True)
+    scenario.verify(rich_community.is_member(alice.address))
+    scenario.verify(~rich_community.is_member(carl.address))
+    rich_community.leave(_sender = bob)
+    scenario.verify(~rich_community.is_member(bob.address))
+    
