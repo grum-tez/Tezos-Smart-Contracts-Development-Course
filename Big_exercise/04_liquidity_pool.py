e0c2fda5eafc70598503bc2ae19b5cf5b907ac17
mathias-hiron <mathias.hiron@gmail.com>
2023-11-26 14:42:17 +0100

    Use Liquidity Pool to trade tokens with Tez

--- Big_exercise/04_liquidity_pool.py
+++ Big_exercise/04_liquidity_pool.py
@@ -2,7 +2,36 @@ import smartpy as sp
 
 @sp.module
 def main():
-    
+    param_type:type = sp.record(source = sp.address, destination = sp.address, amount = sp.nat)
+
+    class Ledger(sp.Contract):
+        def __init__(self, owner, total_supply):
+            self.data.balances = sp.big_map({ owner : total_supply })
+            self.data.allowances = sp.big_map({})
+          
+        @sp.entrypoint
+        def transfer(self, source, destination, amount):
+            if source != sp.sender:
+                allowed = self.data.allowances[(source, sp.sender)]
+                assert allowed >= amount
+                self.data.allowances[(source, sp.sender)] = sp.as_nat(allowed - amount)
+            assert self.data.balances[source] >= amount
+            self.data.balances[source] = sp.as_nat(self.data.balances[source] - amount)
+            if not self.data.balances.contains(destination):
+                self.data.balances[destination] = sp.nat(0)
+            self.data.balances[destination] += amount
+
+        @sp.onchain_view
+        def get_balance(self, user):
+            sp.cast(user, sp.address)
+            return self.data.balances[user]
+
+        @sp.entrypoint
+        def allow(self, operator, amount):
+            if not self.data.allowances.contains((sp.sender, operator)):
+                self.data.allowances[(sp.sender, operator)] = 0
+            self.data.allowances[(sp.sender, operator)] += amount
+
     class LiquidityPool(sp.Contract):
         def __init__(self, owner, ledger):
             self.data.ledger = ledger
@@ -21,7 +50,10 @@ def main():
             self.data.tokens_owned = deposited_tokens
 
             self.data.ledger_contract_opt = sp.contract(param_type, self.data.ledger, entrypoint="transfer")
-            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = deposited_tokens), sp.tez(0), self.data.ledger_contract_opt.unwrap_some())
+            sp.transfer(sp.record(source = sp.sender, destination = sp.self_address(), amount = deposited_tokens),
+                        sp.tez(0),
+                        self.data.ledger_contract_opt.unwrap_some()
+                       )
             
         @sp.entrypoint
         def withdraw_liquidity(self):
@@ -46,7 +78,7 @@ def main():
             sp.send(sp.sender, tez_obtained)
 
         @sp.entrypoint
-        def buyTokens(self, min_tokens_bought):
+        def buy_tokens(self, min_tokens_bought):
             sp.cast(min_tokens_bought, sp.nat)
             tokens_obtained = sp.as_nat(self.data.tokens_owned - sp.fst(sp.ediv(self.data.K, sp.balance).unwrap_some()))
             assert tokens_obtained >= min_tokens_bought
@@ -74,3 +106,28 @@ def main():
             trace("The price of the token returned is:")
             trace(token_price)
             return token_price
+
+
+@sp.add_test()
+def test():
+    alice = sp.test_account("alice")
+    bob = sp.test_account("bob")
+    carl = sp.test_account("carl")
+    scenario = sp.test_scenario("Test" , main)
+    ledger = main.Ledger(owner = alice.address, total_supply = 1000000)
+
+    scenario += ledger
+
+    
+    liquidity_pool = main.LiquidityPool(owner = alice.address, ledger = ledger.address)
+    scenario += liquidity_pool
+
+    ledger.allow(sp.record(operator = liquidity_pool.address, amount = 2000), _sender = alice)
+    liquidity_pool.provide_liquidity(2000, _sender = alice, _amount = sp.tez(2000))
+    
+    liquidity_pool.buy_tokens(90, _sender = bob, _amount = sp.tez(100))
+
+    liquidity_pool.buy_tokens(500, _sender = carl, _amount = sp.tez(1000))
+
+    ledger.allow(sp.record(operator = liquidity_pool.address, amount = 96), _sender = bob)
+    liquidity_pool.sell_tokens(nb_tokens_sold = 96, min_tez_requested = sp.tez(200), _sender = bob)
