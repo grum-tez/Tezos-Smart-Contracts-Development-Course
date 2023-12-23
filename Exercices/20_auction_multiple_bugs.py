a852e0f6650257d22da6dbd1990c1e4decd20f85
mathias-hiron <mathias.hiron@gmail.com>
2023-12-23 13:38:28 +0100

    Update to new syntax

--- Exercices/20_auction_multiple_bugs.py
+++ Exercices/20_auction_multiple_bugs.py
@@ -15,13 +15,11 @@ def main():
             self.data.token_id += 1
         
         @sp.entrypoint
-        def open_auction(self, item_id, seller, deadline):
-            # Bug 1, 2, 3, 4
-            # bug 1
-            self.data.items[item_id] = sp.record(seller = seller,
+        def open_auction(self, item_id, deadline):
+            self.data.items[item_id] = sp.record(seller = sp.sender,
                                                 deadline = deadline,
                                                 top_bid = sp.tez(0),
-                                                top_bidder = seller
+                                                top_bidder = sp.sender
                                                )
     
         @sp.entrypoint
@@ -42,17 +40,18 @@ def main():
             sp.send(item.seller, item.top_bid) # Bug 5
             self.data.tokens[item_id].owner = item.top_bidder # Bug 7
 
-@sp.add_test(name = "Testing extortion attack")
+@sp.add_test()
 def test():
     seller1 = sp.test_account("seller1").address
     seller2 = sp.test_account("seller2").address
     alice = sp.test_account("alice").address
     bob = sp.test_account("bob").address
-    scenario = sp.test_scenario(main)
+    scenario = sp.test_scenario("Test", main)
     auctionContract = main.Auction()
     scenario += auctionContract
     auctionContract.mint("Mon NFT").run(sender = seller1)
     auctionContract.open_auction(item_id = 1, seller = seller1, deadline = sp.timestamp(100))
-    auctionContract.bid(1).run(sender = alice, amount = sp.tez(1), now = sp.timestamp(1))
-    auctionContract.bid(1).run(sender = bob, amount = sp.tez(2), now = sp.timestamp(2))
-    auctionContract.claimtop_bid(1).run(sender = seller1, now = sp.timestamp(101))
+    auctionContract.bid(1, _sender = alice, _amount = sp.tez(1), _now = sp.timestamp(1))
+    auctionContract.bid(1, _sender = bob, _amount = sp.tez(2), _now = sp.timestamp(2))
+    auctionContract.claimtop_bid(1, _sender = seller1, _now = sp.timestamp(101))
+
