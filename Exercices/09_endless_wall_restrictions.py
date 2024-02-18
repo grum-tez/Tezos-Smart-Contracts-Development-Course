da7af4129c6337f0522a131b52f41015be321af1
mathias-hiron <mathias.hiron@gmail.com>
2024-02-18 13:53:12 +0100

    Move contract instanciation

--- Exercices/09_endless_wall_restrictions.py
+++ Exercices/09_endless_wall_restrictions.py
@@ -24,8 +24,8 @@ def test():
    alice=sp.test_account("Alice").address
    bob=sp.test_account("Bob").address
    eve=sp.test_account("Eve").address
-   c1 = main.EndlessWall(initial_text = "Hello", owner=alice)
    scenario = sp.test_scenario("Test", main)
+   c1 = main.EndlessWall(initial_text = "Hello", owner=alice)
    scenario += c1
    scenario.h3("Testing write_message entrypoint ")
    c1.write_message("Ana & Jack", _sender = eve)
