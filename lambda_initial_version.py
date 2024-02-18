67e508c0dc115b54b517caaa462618cd86f27f17
mathias-hiron <mathias.hiron@gmail.com>
2024-02-18 14:49:29 +0100

    Update to new syntax

--- lambda_initial_version.py
+++ lambda_initial_version.py
@@ -29,19 +29,19 @@ def main():
        def changeRule(self, newRule):
            self.data.priceUpdateRule = newRule
 
-@sp.add_test(name='Testing Update Price')
+@sp.add_test()
 def test():
     alice = sp.test_account("alice").address
     bob = sp.test_account("bob").address
     eve = sp.test_account("eve").address
+    scenario = sp.test_scenario("Test", main)
     c1 = main.NftForSale(owner = alice, metadata = "Gwen's nft", price=sp.mutez(5000000))
-    scenario = sp.test_scenario(main)
     scenario +=c1
     scenario.h3(" Testing increasing price")
-    c1.buy().run(sender = bob, amount = sp.mutez(5000000))
-    c1.buy().run(sender = eve, amount = sp.mutez(5500000))
-    c1.buy().run(sender = alice, amount = sp.mutez(6000000), valid = False)
+    c1.buy(_sender = bob, _amount = sp.mutez(5000000))
+    c1.buy(_sender = eve, _amount = sp.mutez(5500000))
+    c1.buy(_sender = alice, _amount = sp.mutez(6000000), _valid = False)
     scenario.verify(c1.data.price == sp.mutez(6050000))
-    c1.changeRule(main.newPrice2).run(sender = alice)
-    c1.buy().run(sender = bob, amount = sp.mutez(6050000))
+    c1.changeRule(main.newPrice2, _sender = alice)
+    c1.buy(_sender = bob, _amount = sp.mutez(6050000))
     scenario.verify(c1.data.price == sp.mutez(7050000))
