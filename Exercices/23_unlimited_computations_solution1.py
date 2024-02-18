b8fbcb2238128bfdffb6f493c301329ad0d44504
mathias-hiron <mathias.hiron@gmail.com>
2024-02-18 14:23:17 +0100

    Update to new syntax

--- Exercices/23_unlimited_computations_solution1.py
+++ Exercices/23_unlimited_computations_solution1.py
@@ -28,20 +28,20 @@ def main():
             sp.send(sp.sender, total)
                    
 
-@sp.add_test(name = "Testing extortion attack")
+@sp.add_test()
 def test():
     alice = sp.test_account("alice").address
     bob = sp.test_account("bob").address
     carl = sp.test_account("carl").address
-    scenario = sp.test_scenario(main)
+    scenario = sp.test_scenario("Test extortion attack", main)
     timeSafeContract = main.TimeSafe(alice)
     scenario += timeSafeContract
-    timeSafeContract.deposit(sp.timestamp(100)).run(sender = bob, amount = sp.tez(10))
-    timeSafeContract.deposit(sp.timestamp(200)).run(sender = carl, amount = sp.tez(20))
-    timeSafeContract.withdraw().run(sender = alice, now = sp.timestamp(0))
+    timeSafeContract.deposit(sp.timestamp(100), _sender = bob, _amount = sp.tez(10))
+    timeSafeContract.deposit(sp.timestamp(200), _sender = carl, _amount = sp.tez(20))
+    timeSafeContract.withdraw(_sender = alice, _now = sp.timestamp(0))
     scenario.verify(timeSafeContract.balance == sp.tez(30))
-    timeSafeContract.withdraw().run(sender = alice, now = sp.timestamp(100))
+    timeSafeContract.withdraw(_sender = alice, _now = sp.timestamp(100))
     scenario.verify(timeSafeContract.balance == sp.tez(20))
-    timeSafeContract.withdraw().run(sender = alice, now = sp.timestamp(2000))
+    timeSafeContract.withdraw(_sender = alice, _now = sp.timestamp(2000))
     scenario.verify(timeSafeContract.balance == sp.tez(0))
     
